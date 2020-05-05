0eff247d0534f897d25f0c9189585edb8c87edea
Raúl <raulguzmanguerrero@outlook.com>
2020-05-05 02:22:34 -0500

    More memory-efficient training algorithm

--- Utilities/train-classifier.py
+++ Utilities/train-classifier.py
@@ -1,6 +1,6 @@
 # This file trains a classifier with a csv file
 
-# This is a command-line utility that uses the following arguments (classifier_file, csv_file)
+# This is a command-line utility that uses the following arguments (classifier_file, csv_file, samples)
 
 import sys
 import csv
@@ -10,12 +10,13 @@ from textblob.classifiers import NaiveBayesClassifier
 arguments = sys.argv
 opened_classifier = None
 
-if len(arguments) == 1:
-    print("This is a command-line utility that uses the following arguments: classifier_file, csv_file")
+if len(arguments) < 3:
+    print("This is a command-line utility that uses the following arguments: classifier_file, csv_file, samples")
     raise Exception("No arguments found")
 
 classifier_file = str(arguments[1])
 csv_file = str(arguments[2])
+samples = str(arguments[3])
 
 # Opening pickled object
 with open(classifier_file, 'rb') as input:
@@ -29,22 +30,28 @@ if type(opened_classifier) != NaiveBayesClassifier:
 csv_data = open(csv_file, newline='', encoding='utf-8')
 
 # Reading csv to convert to the data structure TextBlob requires 
-required_structure = []
 reader = csv.reader(csv_data)
 item_counter = 0
 
-for row in reader:
-    item_counter += 1
-    print("Converting item #{0}".format(item_counter), sep=' ', end='\r', flush=True)
-    required_structure.append((row[0], row[1]))
-
-print("\nDone converting")
-
-# Train classifier
 print("Training...")
-opened_classifier.update(required_structure)
+
+for row in reader:
+    # Stop execution after sample number is reached
+    if item_counter == samples:
+        break
+
+    required_structure = []
+
+    item_counter += 1
+    print("Training item #{0}".format(item_counter), sep=' ', end='\r', flush=True)
+    required_structure.append((row[0], row[1]))
+    
+    # Train classifier
+    opened_classifier.update(required_structure)
+
 
 # Saving updated classifier
+print("Saving {0}...".format(classifier_file))
 with open(classifier_file, 'wb') as output:
     pickle.dump(opened_classifier, output, pickle.HIGHEST_PROTOCOL)
 
